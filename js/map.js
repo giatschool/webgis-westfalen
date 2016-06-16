90b63e8973c6f4caf8540e0f656af915fa95c330
Matthias Pfeil <matthias.pfeil@wwu.de>
2016-06-16 15:03:00 +0200

    moved printer to own module

--- js/map.js
+++ js/map.js
@@ -271,8 +271,10 @@ require(['esri/map',
   'esri/geometry/Extent',
   'esri/SpatialReference',
   'esri/layers/OpenStreetMapLayer',
+  'dijit/form/Button',
+  'data/Printer',
   'dojo/dom-construct',
-  'dojo/domReady!'], function(Map, Popup, SimpleFillSymbol, SimpleLineSymbol, Color, Extent, SpatialReference, OpenStreetMapLayer, domConstruct) {
+  'dojo/domReady!'], function(Map, Popup, SimpleFillSymbol, SimpleLineSymbol, Color, Extent, SpatialReference, OpenStreetMapLayer, Button, Printer, domConstruct) {
 
   addTooltips(); //the mouse-over tooltips are created programmatically
 
@@ -344,20 +346,13 @@ require(['esri/map',
 
   fullExtent();
 
-  // attributionDiv = query('.esriAttributionList');
-  // domConstruct.place('<span class="esriAttributionLastItem" style="display: inline;">&copy; Landschaftsverband Westfalen-Lippe (LWL)<span class="esriAttributionDelim"> | </span></span>', attributionDiv[0]);
-  // esriLogoDiv = query('.logo-med');
-  // logoDiv = domConstruct.create('div',{
-  //   className: 'logo'
-  // }, esriLogoDiv[0], 'before');
-  // domConstruct.place('<a id="logo-ifgi" href="http://ifgi.uni-muenster.de/" target="_blank"></a>', logoDiv);
+  //create printer button
+  var printButton = new Button({
+      onClick: function(){
+          Printer.printMap();
+      }
+  }, 'printMap').startup();
 });
-
-function getColor() {
-  startColor = document.getElementById('colorPickerValueOne').value;
-  endColor = document.getElementById('colorPickerValueTwo').value;
-  return [startColor,endColor];
-}
 
 /**
  * Method for changing the active overlay layer
@@ -467,82 +462,6 @@ function layerChange(layerNr,removeLayer) {
     //updateLayerVisibility();
     updateTimeslider();
   }
-}
-
-/**
- * converts the legend to JSON to transmit it to the print preview
- */
-function legendToJSON() {
-  var i = 0;
-
-  var legend = {};
-  legend.values = [];
-  legend.diagram = [];
-
-  $('div#myLegend table tr').each( function () {
-    legend.values.push(
-      {
-        'bg' : $(this).children('td:nth-of-type(1)').children('.legendColorfield').css('background-color'),
-        'min' : $(this).children('td:nth-of-type(2)').text(),
-        'l' : $(this).children('td:nth-of-type(4)').text(),
-        'max' : $(this).children('td:nth-of-type(5)').text()
-      }
-    );
-  });
-
-  if($('div#legendDiagrams').length > 0) {
-    $('div#legendDiagrams table tbody tr').each( function () {
-      legend.diagram.push(
-        {
-          'icon' : $(this).children('td:nth-of-type(1)').children('img').attr('src'),
-          'text' : $(this).children('td:nth-of-type(2)').text()
-        }
-      );
-    });
-  }
-
-  return legend;
-}
-
-/**
- * function to set the printer, incl. title and author of the map and export it
- */
-function initPrinter(){
-  console.log('initPrinter called');
-
-  var svgElement = $('#map_gc')[0];
-  var xmlSerializer = new XMLSerializer();
-  var str = xmlSerializer.serializeToString(svgElement);
-  var overlayUrl = $('#map_collection img').attr('src');
-
-  var mapTitle = $('#mapExportTitle').val();
-  var mapAuthor = $('#mapExportAuthor').val();
-  var jsonLegend = legendToJSON();
-
-  $('#exportWarning').html('<img src="images/loading_small.gif" id="loadingImage" alt="loading" />');
-
-  $.ajax({
-      type: 'POST',
-      url: './lwl-convert/converter.php',
-      data: {
-        'svg': str,
-        'overlay': overlayUrl,
-        'legend': JSON.stringify(jsonLegend)
-       },
-      success: function(data) {
-        response = $.parseJSON(data);
-        console.log('Map printed, id '+response.message);
-        if(response.status==='success') {
-          $('#exportWarning').html('<a style="margin:" href="./lwl-convert/printpreview.php?map='+response.message+'&name='+escape(mapAuthor)+'&title='+escape(mapTitle)+'" target="_blank">Link zur Druckansicht</a>');
-        } else {
-          $('#exportWarning').html('<i>Beim Erstellen der Druckansicht ist ein Fehler aufgetreten.</i>');
-        }
-      },
-      fail: function(data) {
-        console.log('Error printing id '+response.message);
-        $('#exportWarning').html('<i>Beim Erstellen der Druckansicht ist ein Fehler aufgetreten.</i>');
-      }
-  });
 }
 
 /**
