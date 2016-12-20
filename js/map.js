9207282a9649cd8fac43b344e3166c7a43b06006
mdragunski <m.dragunski@uni-muenster.de>
2016-12-20 18:30:46 +0100

    added initPrinter()

--- js/map.js
+++ js/map.js
@@ -612,5 +612,45 @@ function setFeatureLayerOpacity(opacity) {
   $('.legendColorfield').css({ opacity: opacity });*/
 }
 
+/**
+ * function to set the printer, incl. title and author of the map and export it
+ */
+function initPrinter(){
+    console.log('initPrinter called');
+
+    var svgElement = $('#map_gc')[0];
+    var xmlSerializer = new XMLSerializer();
+    var str = xmlSerializer.serializeToString(svgElement);
+    var overlayUrl = $('#map_collection img').attr('src');
+
+    var mapTitle = $('#mapExportTitle').val();
+    var mapAuthor = $('#mapExportAuthor').val();
+    var jsonLegend = legendToJSON();
+
+    $('#exportWarning').html('<img src="images/loading_small.gif" id="loadingImage" alt="loading" />');
+
+    $.ajax({
+        type: 'POST',
+        url: 'lwl-convert/converter.php',
+        data: {
+            'svg': str,
+            'overlay': overlayUrl,
+            'legend': JSON.stringify(jsonLegend)
+         },
+        success: function(data) {
+            response = $.parseJSON(data);
+            console.log('Map printed, id '+response.message);
+            if(response.status==='success') {
+                $('#exportWarning').html('<a style="margin:" href="lwl-convert/printpreview.php?map='+response.message+'&name='+escape(mapAuthor)+'&title='+escape(mapTitle)+'" target="_blank">Link zur Druckansicht</a>');
+            } else {
+                $('#exportWarning').html('<i>Beim Erstellen der Druckansicht ist ein Fehler aufgetreten.</i>');
+            }
+        },
+        fail: function(data) {
+            console.log('Error printing id '+response.message);
+            $('#exportWarning').html('<i>Beim Erstellen der Druckansicht ist ein Fehler aufgetreten.</i>');
+        }
+    });
+}
 
 /* jshint ignore:end */
