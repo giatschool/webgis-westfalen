b94b02c01f6e18fe9402d46e68cc08d7f1db03fe
Marc Dragunski <m_drag01@geoinformatik.geo.uni-muenster.de>
2017-06-06 10:52:26 +0200

    server migration

--- lwl-convert/config.php
+++ lwl-convert/config.php
@@ -1,13 +1,13 @@
 <?php
 // Absolute path to input & output folders here
 // Output should be reachable by HTTP server
-$temp_folder = '/srv/htdocs/LWL-GIS/temp/';
+$temp_folder = 'C:/www/webgis-westfalen/lwl-convert/temp/';
 $folder_in = $temp_folder.'in/';
 $folder_out = $temp_folder.'out/';
 
 // Relative path to output folder to printpreview.php
 $folder_out_rel = 'temp/out/';
 
-$imagick_folder = "/usr/bin/ImageMagick/";
+$imagick_folder = "C:\ImageMagick-7.0.5-Q16";
 $webgis_url = "http://www.webgis-westfalen.de/";
 ?>
